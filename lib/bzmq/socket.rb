63a32e42d3fa50b7f3c0c3332d54f0cdb6f0a2b3
Joel Segerlind <joel@burtcorp.com>
2013-02-04 17:33:52 +0100

    Add errno to SocketErrors

--- lib/bzmq/socket.rb
+++ lib/bzmq/socket.rb
@@ -1,11 +1,9 @@
 
 module BZMQ
   class Socket
-    SocketError = Class.new(StandardError)
-
     def initialize(type, options={})
-      @context = Context.context(options[:io_threads] || 1)
-      @socket = @context.socket(type)
+      context = Context.context(options[:io_threads] || 1)
+      @socket = context.socket(type)
       raise SocketError unless @socket
       error_check do
         @socket.setsockopt ZMQ::LINGER, options[:linger] || -1
@@ -43,8 +41,18 @@ module BZMQ
     end
 
     def error_check(&block)
-      raise SocketError, ZMQ::Util.error_string unless ZMQ::Util.resultcode_ok? block.call
+      unless ZMQ::Util.resultcode_ok? block.call
+        raise SocketError.new(ZMQ::Util.error_string, ZMQ::Util.errno)
+      end
       true
+    end
+  end
+
+  class SocketError < StandardError
+    attr_reader :error_code
+    def initialize(message, error_code=-1)
+      @error_code = error_code
+      super(message)
     end
   end
 end
