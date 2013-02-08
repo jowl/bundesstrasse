3b680bd0d7fd62a35c3943f84628e0d2d2ffd015
Joel Segerlind <joel@burtcorp.com>
2013-02-08 14:42:32 +0100

    Add functionality do QueueDevice

--- lib/bundesstrasse/devices.rb
+++ lib/bundesstrasse/devices.rb
@@ -1,13 +1,19 @@
 module Bundesstrasse
   class QueueDevice < Device
-    def initialize(frontend, backend)
+    attr_reader :context
+    def initialize(context, frontend, backend)
       super(ZMQ::QUEUE, frontend, backend)
+      @context = context
     end
     
+    def create_endpoint!(options={})
+      @context.socket(RepSocket, options)
+    end
+
     def self.create(context)
       frontend = context.socket(RouterSocket)
       backend = context.socket(DealerSocket)
-      new(frontend, backend)
+      new(context, frontend, backend)
     end
   end
 
